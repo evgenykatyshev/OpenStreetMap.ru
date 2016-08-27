09a024ac0b3e3a90d8289a6118c005c537367f98
evgenykatyshev <me@ekatyshev.ru>
2016-08-27 15:53:22 +0300

    Новая страница карт для навигаторов
    Позже добавлю Maps.me и Osmand

--- www/navigator.php
+++ www/navigator.php
@@ -17,18 +17,44 @@ PHP_TOPBAR;
 $page_content = <<<PHP_CONTENT
 
 <div id="ncontent">
-  <img src="/img/navigators.png" usemap="#navigators">
-  <map id="navigators" name="navigators">
-    <area id="garmin" shape="rect" coords="335,1,625,85" href="http://gis-lab.info/data/mp" target="blank">
-    <area id="navitel" shape="rect" coords="295,155,634,250" href="http://navitel.osm.rambler.ru/" target="blank">
-    <area id="cityguide" shape="rect" coords="230,415,395,535" href="http://peirce.gis-lab.info/daily" target="blank">
-    <area id="osmand" shape="rect" coords="1,1,275,95" href="http://osmand.net/" target="blank">
-    <area id="gisrussa" shape="rect" coords="435,405,535,530" href="http://osm-russa.narod.ru/" target="blank">
-    <area id="pocketgis" shape="rect" coords="1,105,250,235" href="http://probki77.ru/pgs/russia.php" target="blank">
-    <area id="autosputnik" shape="rect" coords="320,270,615,390" href="http://openstreetmaps.ru/" target="blank">
-    <area id="navikey" shape="rect" coords="15,270,255,355" href="http://www.megamaps.org/" target="blank">
-    <area id="progorod" shape="rect" coords="75,415,190,530" href="http://forum.pro-gorod.ru/viewtopic.php?f=190&t=712" target="blank">
-  </map>
+  <h1>Карты для навигаторов</h1>
+	<p class="description">Карты России и других стран на основе OpenStreetMap доступны для популярных навигаторов и навигационных программ.</p>
+	<div class="nav-system nav-system_garmin">
+	  <div class="nav-system__logo"></div>
+	  <div class="nav-system__title">Ежедневно обновляемые карты для Garmin</div>
+		<div>
+		  <a href="http://gis-lab.info/data/mp/" class="nav-system__link">gis-lab.info</a>
+		  <a href="http://maptourist.org/file/category/3-garmin" class="nav-system__link">maptourist.org</a>
+		  <a href="http://velo100.ru/gps/download" class="nav-system__link">velo100.ru</a>
+		</div>
+	</div>
+	<div class="nav-system nav-system_navitel">
+	  <div class="nav-system__logo"></div>
+	  <a href="http://navitel.osm.rambler.ru/" class="nav-system__title">Navitel</a>
+		<div class="nav-system__updates">для версий до 5.0.3, лучше 3.5<br />обновление раз в неделю</div>
+	</div>
+	<div class="nav-system nav-system_pocketgis">
+	  <div class="nav-system__logo"></div>
+	  <a href="http://boltomerka.ru/pgsmaps.php" class="nav-system__title">PocketGIS</a>
+		<div class="nav-system__updates">обновление раз в полгода</div>
+	</div>
+	<div class="nav-system nav-system_cityguide">
+	  <div class="nav-system__logo"></div>
+	  <a href="http://peirce.gis-lab.info/daily" class="nav-system__title">Ситигид</a>
+		<div class="nav-system__updates">обновление раз в неделю</div>
+	</div>
+	<div class="nav-system nav-system_progorod">
+	  <div class="nav-system__logo">ПРОГОРОД</div>
+	  <a href="http://www.pro-gorod.ru/download#maps" class="nav-system__title">Прогород</a>
+		<div class="nav-system__updates">за 2015 год</div>
+	</div>
+	<div class="nav-system nav-system_7ways">
+	  <div class="nav-system__logo">Семь дорог</div>
+	  <div class="nav-system__title">Семь дорог</div>
+		<div class="nav-system__updates">обновление раз в неделю</div>
+	  <a href="http://www.megamaps.org/" class="nav-system__link">megamaps.org</a>
+	  <a href="http://navitel.osm.rambler.ru/" class="nav-system__link">osm.rambler.ru</a>
+	</div>
 </div>
 
 PHP_CONTENT;
